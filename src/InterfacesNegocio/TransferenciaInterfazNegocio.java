3b7849d7de4efb5f2347c3651a53b436c8952603
Mateo De Benedictis <mateodebenedictismazza@gmail.com>
2021-07-20 01:24:10 -0300

    Transferencia Negocio e Implementacion

--- src/InterfacesNegocio/TransferenciaInterfazNegocio.java
+++ src/InterfacesNegocio/TransferenciaInterfazNegocio.java
@@ -1,5 +1,21 @@
 package InterfacesNegocio;
 
+import java.util.List;
+
+import DAO.TransferenciaDAO;
+import Entidad.Cuentas;
+import Entidad.Transferencias;
+
 public interface TransferenciaInterfazNegocio {
 
+	public TransferenciaDAO getTransferenciaDao();
+	
+	public void setTransferenciaDao(TransferenciaDAO TransferenciaDAO);
+	
+	public void agregarTransferencia(Cuentas cuenta_origen, Cuentas cuenta_destino, float importe, String detalle);
+	
+	public List<Transferencias> getListaDeTransferenciasByCuenta(Cuentas cuenta);
+	
+	public void prueba ();
+	
 }
