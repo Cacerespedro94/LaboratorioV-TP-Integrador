6aab0d9c26c1aa9cbc8d6ef38e7f0376d222f6d7
Cacerespedro94 <cacerespedro94@gmail.com>
2021-07-20 01:15:56 -0300

    Se hizo cuenta negocio y cuenta negocio interfaz

--- src/Negocio/CuentaNegocio.java
+++ src/Negocio/CuentaNegocio.java
@@ -1,12 +1,41 @@
 package Negocio;
 
+import java.util.List;
+
 import DAO.CuentaDAO;
 import Entidad.Cuentas;
+import InterfacesNegocio.CuentaInterfazNegocio;
 
-public class CuentaNegocio {
-		
+public class CuentaNegocio implements CuentaInterfazNegocio {
+	 CuentaDAO dao=new CuentaDAO();
 	public void altaCuenta(Cuentas cuenta) {
-		 CuentaDAO dao=new CuentaDAO();
+	
     	dao.altaCuenta(cuenta);
+	}
+
+	@Override
+	public void modificarCuenta(Cuentas cuenta) {
+		// TODO Auto-generated method stub
+		dao.modificarCuenta(cuenta);
+	}
+
+	@Override
+	public void eliminarCuenta(Cuentas cuenta) {
+		// TODO Auto-generated method stub
+		dao.eliminarCuenta(cuenta);
+	}
+
+	@Override
+	public List<Cuentas> obtenerCuentasPorCliente(int idCliente) {
+		// TODO Auto-generated method stub
+		List<Cuentas> listaCuentas = dao.obtenerCuentasPorCliente(idCliente);
+		return listaCuentas;
+	}
+
+	@Override
+	public Cuentas obtenerCuenta(String cbu) {
+		// TODO Auto-generated method stub
+		Cuentas cuenta = dao.obtenerCuenta(cbu);
+		return cuenta;
 	}
 }
