23a4a4434843488450a8e409e8aa82c781ed8456
Cacerespedro94 <cacerespedro94@gmail.com>
2021-07-20 00:09:14 -0300

    Se hace el eliminar dao cuenta

--- src/DAO/CuentaDAO.java
+++ src/DAO/CuentaDAO.java
@@ -7,8 +7,12 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 import Entidad.Cuentas;
+import dao.ConfigHibernate;
+import dao.Query;
+import entidades.Cuenta;
 public class CuentaDAO {
-	
+	private static ConfigHibernate ch;
+	public static Session session;
 	public void altaCuenta(Cuentas cuenta) {
 		
 	/*	Configuration configuration = new Configuration();
@@ -23,5 +27,23 @@ public class CuentaDAO {
 		session.getTransaction().commit();
 		session.close();
 	}
+	
+    public void eliminarCuenta(Cuentas cuenta) {
+		
+		
+		Integer num_cuenta = cuenta.getNroCuenta();
+		
+		session = ch.getConexion();
+		session.beginTransaction();
+   	
+		String hql = "UPDATE Cuenta SET estado = false WHERE nroCuenta = :nroCuenta";
+		
+		Query q = session.createQuery(hql);
+		q.setParameter("nroCuenta", nroCuenta);
+	    q.executeUpdate();
+	    
+		session.getTransaction().commit();
+	}
+   
 	
 }
